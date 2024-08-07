b7b102814b8f7bcc57151edc921f771a350bf2ec
CodyKoz <Cody.koz@comcast.net>
2024-08-06 19:11:39 -0500

    implementing gameplay loop

--- Minesweeper.c
+++ Minesweeper.c
@@ -102,6 +102,8 @@ void set_bombs(struct game_board* game_board_data) {
             
             }
 
+            break;
+
 
         case(EASY):
 
@@ -136,6 +138,8 @@ void set_bombs(struct game_board* game_board_data) {
             
             }
 
+            break;
+
         case(MEDIUM):
 
             srand(time(0));;
@@ -169,6 +173,8 @@ void set_bombs(struct game_board* game_board_data) {
             
             }
 
+            break;
+
         case(HARD):
 
             srand(time(0));
@@ -201,6 +207,8 @@ void set_bombs(struct game_board* game_board_data) {
                 }
             
             }
+
+            break;
 
     }
 }
@@ -282,6 +290,7 @@ void* create_game_board_array(enum difficulty game_difficulty) {
                 for(int y = 0; y < game_array->rows; y++) {
 
                     game_array->board_ptr[x].cells_ptr[y].c_stat = HIDDEN;
+                    game_array->board_ptr[x].cells_ptr[y].visited = FALSE;
                     game_array->board_ptr[x].cells_ptr[y].display_char = ' ';
 
                 }
@@ -290,7 +299,7 @@ void* create_game_board_array(enum difficulty game_difficulty) {
 
             //call set_bombs function to complete board setup.
             set_bombs(game_array);
-
+            break;
 
         case(EASY):
 
@@ -351,7 +360,7 @@ void* create_game_board_array(enum difficulty game_difficulty) {
 
             //call set_bombs function to complete board setup.
             set_bombs(game_array);
-
+            break;
 
         case(MEDIUM):
 
@@ -412,7 +421,7 @@ void* create_game_board_array(enum difficulty game_difficulty) {
 
             //call set_bombs function to complete board setup.
             set_bombs(game_array);
-
+            break;
 
         case(HARD):
 
@@ -473,7 +482,7 @@ void* create_game_board_array(enum difficulty game_difficulty) {
 
             //call set_bombs function to complete board setup.
             set_bombs(game_array);
-
+            break;
     }
 
     return game_array;
@@ -484,13 +493,11 @@ void* create_game_board_array(enum difficulty game_difficulty) {
  * 
  * @brief prints the board to the screen, populated with the game data.
  * 
- * @param game_difficulty the difficulty will determine the size of the board that is printed
  * @param game_board_data the array that stores the data for the game that will be played.
  * 
  * @return returns nothing
  * 
  */
-// TOADD - param: struct game_board game_board_data
 void print_board(struct game_board* game_board_arr) {
 
     switch(game_board_arr->board_difficulty) {
@@ -498,15 +505,15 @@ void print_board(struct game_board* game_board_arr) {
             printf("game difficulty: SIMPLE\n\n");
             printf("     0   1   2   3   4\n");
             printf("   /-------------------\\");
-            printf("\n 0 |   |   |   |   |   |");
+            printf("\n 0 | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[0].display_char, game_board_arr->board_ptr[1].cells_ptr[0].display_char, game_board_arr->board_ptr[2].cells_ptr[0].display_char, game_board_arr->board_ptr[3].cells_ptr[0].display_char, game_board_arr->board_ptr[4].cells_ptr[0].display_char);
             printf("\n   |---|---|---|---|---|");
-            printf("\n 1 |   |   |   |   |   |");
+            printf("\n 1 | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[1].display_char, game_board_arr->board_ptr[1].cells_ptr[1].display_char, game_board_arr->board_ptr[2].cells_ptr[1].display_char, game_board_arr->board_ptr[3].cells_ptr[1].display_char, game_board_arr->board_ptr[4].cells_ptr[1].display_char);
             printf("\n   |---|---|---|---|---|");
-            printf("\n 2 |   |   |   |   |   |");
+            printf("\n 2 | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[2].display_char, game_board_arr->board_ptr[1].cells_ptr[2].display_char, game_board_arr->board_ptr[2].cells_ptr[2].display_char, game_board_arr->board_ptr[3].cells_ptr[2].display_char, game_board_arr->board_ptr[4].cells_ptr[2].display_char);
             printf("\n   |---|---|---|---|---|");
-            printf("\n 3 |   |   |   |   |   |");
+            printf("\n 3 | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[3].display_char, game_board_arr->board_ptr[1].cells_ptr[3].display_char, game_board_arr->board_ptr[2].cells_ptr[3].display_char, game_board_arr->board_ptr[3].cells_ptr[3].display_char, game_board_arr->board_ptr[4].cells_ptr[3].display_char);
             printf("\n   |---|---|---|---|---|");
-            printf("\n 4 |   |   |   |   |   |");
+            printf("\n 4 | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[4].display_char, game_board_arr->board_ptr[1].cells_ptr[4].display_char, game_board_arr->board_ptr[2].cells_ptr[4].display_char, game_board_arr->board_ptr[3].cells_ptr[4].display_char, game_board_arr->board_ptr[4].cells_ptr[4].display_char);
             printf("\n   \\-------------------/");
 
             break;
@@ -515,23 +522,23 @@ void print_board(struct game_board* game_board_arr) {
             printf("game difficulty: EASY\n");
             printf("     0   1   2   3   4   5   6   7   8\n");
             printf("   /-----------------------------------\\");
-            printf("\n 0 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 0 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[0].display_char, game_board_arr->board_ptr[1].cells_ptr[0].display_char, game_board_arr->board_ptr[2].cells_ptr[0].display_char, game_board_arr->board_ptr[3].cells_ptr[0].display_char, game_board_arr->board_ptr[4].cells_ptr[0].display_char, game_board_arr->board_ptr[5].cells_ptr[0].display_char, game_board_arr->board_ptr[6].cells_ptr[0].display_char, game_board_arr->board_ptr[7].cells_ptr[0].display_char, game_board_arr->board_ptr[8].cells_ptr[0].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 1 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 1 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[1].display_char, game_board_arr->board_ptr[1].cells_ptr[1].display_char, game_board_arr->board_ptr[2].cells_ptr[1].display_char, game_board_arr->board_ptr[3].cells_ptr[1].display_char, game_board_arr->board_ptr[4].cells_ptr[1].display_char, game_board_arr->board_ptr[5].cells_ptr[1].display_char, game_board_arr->board_ptr[6].cells_ptr[1].display_char, game_board_arr->board_ptr[7].cells_ptr[1].display_char, game_board_arr->board_ptr[8].cells_ptr[1].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 2 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 2 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[2].display_char, game_board_arr->board_ptr[1].cells_ptr[2].display_char, game_board_arr->board_ptr[2].cells_ptr[2].display_char, game_board_arr->board_ptr[3].cells_ptr[2].display_char, game_board_arr->board_ptr[4].cells_ptr[2].display_char, game_board_arr->board_ptr[5].cells_ptr[2].display_char, game_board_arr->board_ptr[6].cells_ptr[2].display_char, game_board_arr->board_ptr[7].cells_ptr[2].display_char, game_board_arr->board_ptr[8].cells_ptr[2].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 3 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 3 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[3].display_char, game_board_arr->board_ptr[1].cells_ptr[3].display_char, game_board_arr->board_ptr[2].cells_ptr[3].display_char, game_board_arr->board_ptr[3].cells_ptr[3].display_char, game_board_arr->board_ptr[4].cells_ptr[3].display_char, game_board_arr->board_ptr[5].cells_ptr[3].display_char, game_board_arr->board_ptr[6].cells_ptr[3].display_char, game_board_arr->board_ptr[7].cells_ptr[3].display_char, game_board_arr->board_ptr[8].cells_ptr[3].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 4 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 4 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[4].display_char, game_board_arr->board_ptr[1].cells_ptr[4].display_char, game_board_arr->board_ptr[2].cells_ptr[4].display_char, game_board_arr->board_ptr[3].cells_ptr[4].display_char, game_board_arr->board_ptr[4].cells_ptr[4].display_char, game_board_arr->board_ptr[5].cells_ptr[4].display_char, game_board_arr->board_ptr[6].cells_ptr[4].display_char, game_board_arr->board_ptr[7].cells_ptr[4].display_char, game_board_arr->board_ptr[8].cells_ptr[4].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 5 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 5 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[5].display_char, game_board_arr->board_ptr[1].cells_ptr[5].display_char, game_board_arr->board_ptr[2].cells_ptr[5].display_char, game_board_arr->board_ptr[3].cells_ptr[5].display_char, game_board_arr->board_ptr[4].cells_ptr[5].display_char, game_board_arr->board_ptr[5].cells_ptr[5].display_char, game_board_arr->board_ptr[6].cells_ptr[5].display_char, game_board_arr->board_ptr[7].cells_ptr[5].display_char, game_board_arr->board_ptr[8].cells_ptr[5].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 6 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 6 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[6].display_char, game_board_arr->board_ptr[1].cells_ptr[6].display_char, game_board_arr->board_ptr[2].cells_ptr[6].display_char, game_board_arr->board_ptr[3].cells_ptr[6].display_char, game_board_arr->board_ptr[4].cells_ptr[6].display_char, game_board_arr->board_ptr[5].cells_ptr[6].display_char, game_board_arr->board_ptr[6].cells_ptr[6].display_char, game_board_arr->board_ptr[7].cells_ptr[6].display_char, game_board_arr->board_ptr[8].cells_ptr[6].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 7 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 7 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[7].display_char, game_board_arr->board_ptr[1].cells_ptr[7].display_char, game_board_arr->board_ptr[2].cells_ptr[7].display_char, game_board_arr->board_ptr[3].cells_ptr[7].display_char, game_board_arr->board_ptr[4].cells_ptr[7].display_char, game_board_arr->board_ptr[5].cells_ptr[7].display_char, game_board_arr->board_ptr[6].cells_ptr[7].display_char, game_board_arr->board_ptr[7].cells_ptr[7].display_char, game_board_arr->board_ptr[8].cells_ptr[7].display_char);
             printf("\n   |---|---|---|---|---|---|---|---|---|");
-            printf("\n 8 |   |   |   |   |   |   |   |   |   |");
+            printf("\n 8 | %c | %c | %c | %c | %c | %c | %c | %c | %c |", game_board_arr->board_ptr[0].cells_ptr[8].display_char, game_board_arr->board_ptr[1].cells_ptr[8].display_char, game_board_arr->board_ptr[2].cells_ptr[8].display_char, game_board_arr->board_ptr[3].cells_ptr[8].display_char, game_board_arr->board_ptr[4].cells_ptr[8].display_char, game_board_arr->board_ptr[5].cells_ptr[8].display_char, game_board_arr->board_ptr[6].cells_ptr[8].display_char, game_board_arr->board_ptr[7].cells_ptr[8].display_char, game_board_arr->board_ptr[8].cells_ptr[8].display_char);
             printf("\n   \\-----------------------------------/");
             break;
 
@@ -616,10 +623,137 @@ void print_board(struct game_board* game_board_arr) {
 
 }
 
+/**
+ * 
+ * 
+ * 
+ */
+void reveal_cell_recur() {
+
+}
 
 /**
  * 
- * @brief this funtion will execute the game loop, calling other functions for the game until the game state is 'won' or 'lost'.
+ * @brief will iterate through whole board and reset each cell's 'visited' data member to FALSE (0)
+ * 
+ * @return returns nothing
+ * 
+ */
+void reset_visit_flags(struct game_board* game_board_arr) {
+
+    for (int i = 0; i < game_board_arr->cols; i++) {
+
+        for (int j = 0; j < game_board_arr->cols; j++) {
+
+            game_board_arr->board_ptr[i].cells_ptr[j].visited = FALSE;
+
+        }
+
+    }
+
+}
+
+
+/**
+ * 
+ * @brief will house the functions that reveal the cell at the input coordinates, as well as call the 
+ * recursive function to reveal any adjacent cells to the input. will also call function to reset all visited flags. 
+ * 
+ * @param game_board_arr pointer to the game board matrix.
+ * @param col_in column of selected cell coordinates.
+ * @param row_in row of selected cell coordinates.
+ * 
+ */
+void reveal_cell_init(struct game_board* game_board_arr, int col_in, int row_in) {
+
+}
+
+
+/**
+ * 
+ * @brief will change the display character of all cells that have a bomb to '#'.
+ * 
+ * @return returns nothing.
+ * 
+ */
+void reveal_bombs(struct game_board* game_board_arr) {
+
+    for (int i = 0; i < game_board_arr->cols; i++) {
+
+        for (int j = 0; j < game_board_arr->cols; j++) {
+
+            if(game_board_arr->board_ptr[i].cells_ptr[j].has_bomb == TRUE) {
+                game_board_arr->board_ptr[i].cells_ptr[j].c_stat = OPEN;
+                game_board_arr->board_ptr[i].cells_ptr[j].display_char = '#';
+            }
+
+        }
+    }
+
+}
+
+
+/**
+ * 
+ * @brief will determine if game has been won by checking if every cell that does not have a bomb is revealed
+ * 
+ * 
+ * 
+ */
+int win_check(struct game_board* game_board_arr) {
+
+    int open_cell_count;
+
+    switch(game_board_arr->board_difficulty) {
+
+        case(SIMPLE):
+
+            for (int i = 0; i < game_board_arr->cols; i++) {
+
+                for (int j = 0; j < game_board_arr->cols; j++) {
+
+                    if(game_board_arr->board_ptr[i].cells_ptr[j].has_bomb = FALSE && game_board_arr->board_ptr[i].cells_ptr[j].c_stat == OPEN) {
+
+                        open_cell_count++;
+                        
+                    }
+
+                    //checking if number of open cells matches the number of cells on the board minus the number of bombs
+                    if(open_cell_count == 25 - game_board_arr->number_of_bombs) {
+                        game_board_arr->g_stat = WON;
+                        break;
+                    }
+
+                    if(game_board_arr->g_stat == WON) {
+                        break;
+                    }
+
+                }
+
+                if(game_board_arr->g_stat == WON) {
+                    break;
+                }
+
+            }   
+
+            break;
+
+        case(EASY):
+            break;
+
+        case(MEDIUM):
+            break;
+
+        case(HARD):
+            break;
+
+    }
+}
+
+
+/**
+ * 
+ * @brief this funtion will house the game loop, calling other functions for the game until the game state is 'won' or 'lost'.
  * 
  * @param game_board_arr the array that stores the data for the game that will be played.
  * 
@@ -628,10 +762,65 @@ void print_board(struct game_board* game_board_arr) {
  */
 void play_game(struct game_board* game_board_arr) {
 
+    int round_count = 1;
+
+    int col_in;
+    int row_in;
+    char command_in;
+
+    game_board_arr->g_stat = PLAYING;
+
     //game cycle will continue until game status is 'won'
-    while (game_board_arr->g_stat != WON) {
+    while (game_board_arr->g_stat == PLAYING || round_count < 5) {
+
+        //take user input (coordinates, and a command char)
+        printf("\n\nInput cell coordinates (column, then row), and a command for the cell. Example: '3,2,R' \n");
+        printf("commands: | 'R' = reveal cell | 'F' = flag cell |\n");
+
+        scanf("%d,%d,%c", &col_in, &row_in, &command_in);
+
+        //error handling for user input
+        if(command_in != 'f' || command_in != 'F' || command_in != 'r' || command_in != 'R') {
+            printf("Input command invalid. please try again.");
+            break;
+        }
+        else if(col_in < 0 || col_in > game_board_arr->cols || row_in < 0 || row_in > game_board_arr->rows) {
+            printf("Input coordinates out of bounds. please try again.");
+            break;
+        }
+        else {
+
+            //handling flagging cells
+            if(command_in == 'F' || command_in == 'f') {
+
+                game_board_arr->board_ptr[col_in].cells_ptr[row_in].display_char = '!';
+                print_board(game_board_arr);
+
+            }
+            //handling revealing a cell with a bomb
+            else if(game_board_arr->board_ptr[col_in].cells_ptr[row_in].has_bomb == TRUE) {
+
+                game_board_arr->g_stat = LOST;
+                //call funtion to reveal all bombs
+                reveal_bombs(game_board_arr);
+                print_board(game_board_arr);
+
+            }
+            //handling revealing a cell with no bomb
+            else {
+
+                 
+
+            }
 
 
+        }
+
+        
+
+
+
+        round_count++;
 
     }
 
@@ -642,5 +831,7 @@ int main() {
 enum difficulty game_difficulty = select_difficulty();
 struct game_board* game_board_ptr = create_game_board_array(game_difficulty);
 print_board(game_board_ptr);
+
+play_game(game_board_ptr);
 
 };
--- Minesweeper.h
+++ Minesweeper.h
@@ -1,3 +1,5 @@
+#ifndef Minesweeper_H_
+#define Minesweeper_H_
 
 #define TRUE 1
 #define FALSE 0
@@ -23,12 +25,12 @@ enum game_status {
     LOST,
 };
 
-
-
 struct game_cell {
+
     enum cell_status c_stat;
     int has_bomb;
     char display_char;
+    int visited;
 
 };
 
@@ -39,19 +41,28 @@ struct dynamic_array {
 };
 
 struct game_board {
+
     int rows;
     int cols;
     int number_of_bombs;
     enum difficulty board_difficulty;
     enum game_status g_stat;
     struct dynamic_array* board_ptr;
-    
+
 };
 
 int select_difficulty();
 void* create_game_board_array();
 void set_bombs();
-void print_board();               
-void reveal_cell_init();        //
+void print_board();
 void reveal_cell_recur();       //
+void reset_visit_flags();
+void reveal_cell_init();        //
+void reveal_bombs();
+int win_check();
 void play_game();
+
+
+
+
+#endif /* End Minesweeper_H_ */
